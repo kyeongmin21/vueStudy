d7d5ccfc7ae4449e6ba88751fb8a877ea0cf18ee
kim kyeong min <kyeongmin21@naver.com>
2022-03-08 20:53:09 +0900

    component 추가

---
+++ src/components/Button.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    버튼이름 :
+    <button @click="onClick()">{{ btn }}</button>
+    <br>
+
+    Emit: 입력
+    <input type="text" v-model="value" />
+    <button @click="onEmit">{{ name }} Emit 버튼이야~~~</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ComponentButton",
+  props: {
+    // 먼저 아래와 같이 props로 받은 정보가 어떤 자료형인지 정의합니다.
+    // 정의한 경우 정의된 자료형과 다른 자료형의 데이터가 들어오면 warning을 console에 띄웁니다.
+    name: String,
+  },
+  data() {
+    return {
+      btn: "공통 버튼 클릭",
+      value: "",
+    };
+  },
+  methods: {
+    onClick() {
+      console.log("공통버튼 클릭");
+    },
+    onEmit() {
+      let text = "상위컴포넌트로 전달 받은 데이터";
+      this.$emit("setInput", this.value, text);
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div>
+      {{ this.childValue }}
+    </div>
+
+    <button @click="updateParentValue">
+      클릭시 부모의 데이터 값이 증가합니다.
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChildComponent",
+  props: {
+    childValue: {
+      type: Number,
+    },
+  },
+  data() {
+    return {
+      lll: this.childValue,
+    };
+  },
+  methods: {
+    updateParentValue() {
+      this.lll = 1;
+      this.$emit("childEvent", this.lll);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>

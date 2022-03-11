6aa1382f777c86020149bec96c6f37e2d560ed74
kim kyeong min <kyeongmin21@naver.com>
2022-03-11 21:18:39 +0900

    @click 이벤트

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -3,7 +3,8 @@
     <h1>이벤트 핸들링</h1>
 
     <section class="first">
-
+      <button @click="counter">버튼</button>
+      <p>위 버튼을 클릭한 횟수는 {{ num}} 번 입니다.</p>
     </section>
   </div>
 </template>
@@ -17,11 +18,13 @@ export default {
   },
   data() {
     return {
-
+      num: 0,
     };
   },
   methods: {
-
+    counter() {
+      this.num += 1;
+    }
   },
 };
 </script>

9a7d827562dcf2839ccde81721288fdc841a9460
kim kyeong min <kyeongmin21@naver.com>
2022-03-12 13:16:21 +0900

    menu 수정

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="about">
     <div id="nav">
-      <h2><router-link to="/">1. Home</router-link></h2>
-      <h2><router-link to="/menu">2. Menu</router-link></h2>
-      <h2><router-link to="/model">3. v-model</router-link></h2>
-      <h2><router-link to="/if">4. v-if</router-link></h2>
-      <h2><router-link to="/class">5. :class</router-link></h2>
-      <h2><router-link to="/compo">6. component</router-link></h2>
-      <h2><router-link to="/parent">7. parent</router-link></h2>
-      <h2><router-link to="/page" tag="a">8. 페이지 이동</router-link></h2>
-      <h2><router-link to="/event" tag="a">9. 이벤트 핸들링</router-link></h2>
+      <ul class="category">
+        <li><h2><router-link to="/model">1. v-model</router-link></h2></li>
+        <li><h2><router-link to="/if">2. v-if</router-link></h2></li>
+        <li><h2><router-link to="/class">3. :class</router-link></h2></li>
+        <li><h2><router-link to="/compo">4. component</router-link></h2></li>
+        <li><h2><router-link to="/parent">5. parent</router-link></h2></li>
+      </ul>
+      <ul class="category">
+        <li><h2><router-link to="/page" tag="a">6. 페이지 이동</router-link></h2></li>
+        <li><h2><router-link to="/event" tag="a">7. 이벤트 핸들링</router-link></h2></li>
+      </ul>
     </div>
   </div>
 </template>

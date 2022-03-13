6b842162e223f72befdbed50a928424ce204d015
kim kyeong min <kyeongmin21@naver.com>
2022-03-13 12:26:38 +0900

    watched 속성

--- src/views/Watch.vue
+++ src/views/Watch.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <h1>Watch 속성</h1>
-
+    <p>{{ message }}</p>
+    <button @click="changeMessage">click</button>
+    <p>{{ update }}</p>
   </div>
 </template>
 
@@ -9,8 +11,20 @@
 export default {
   data() {
     return {
-
+      message: '안녕하세요',
+      update: '네',
     };
+  },
+  methods: {
+    changeMessage() {
+      this.message = '안녕히가세요'
+    }
+  },
+  watch: {
+    message(newVal, oldVal) {
+      console.log(newVal, oldVal); // 안녕히가세요 , 안녕하세요
+      this.update = '아니오'
+    }
   },
 
 };

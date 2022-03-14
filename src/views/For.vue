4822e244f9b5d750aa875d839e1a1b4d7704aa6e
kyeongmin1011 <kyeongmin@safety-as.com>
2022-03-14 18:06:17 +0900

    v-for todoList 추가 삭제하기

--- src/views/For.vue
+++ src/views/For.vue
@@ -4,19 +4,38 @@
     <section>
       <h3>v-for로 엘리먼트에 배열 매핑하기</h3>
       <div v-for="(item, index) in array" :key="index">
-       {{ index + 1}} : {{ item.fruit }}
+        {{ index + 1 }} : {{ item.fruit }}
       </div>
     </section>
+
     <section>
       <h3>v-for와 객체</h3>
       <div v-for="(value, key, index) in object" :key="value">
         {{ value }}, {{ key }}, {{ index }}
       </div>
     </section>
+
     <section>
       <h3>필터링 / 정렬 된 결과 표시하기</h3>
       <li v-for="n in evenNumber" :key="n">{{ n }}</li>
     </section>
+
+    <section>
+      <h3>todo list</h3>
+      <form @submit.prevent="addNewTodo">
+        <label for="new-todo">해야할 목록 : </label>
+        <input type="text"
+               v-model="newTodoText"
+               id="new-todo"
+               placeholder="리스트를 적어주세요"/>
+        <button>추가</button>
+      </form>
+      <p v-for="(todo, index) in todos" :key="index">
+        {{ todo.title }}
+        <button @click="remove(index)">삭제</button>
+      </p>
+    </section>
+
   </div>
 </template>
 
@@ -24,10 +43,11 @@
 export default {
   data() {
     return {
+      items: ['a', 'b', 'c'],
       array: [
-        { fruit: 'apple' },
-        { fruit: 'banana' },
-        { fruit: 'mango' }
+        {fruit: 'apple'},
+        {fruit: 'banana'},
+        {fruit: 'mango'}
       ],
       object: {
         title: '제목',
@@ -35,7 +55,15 @@ export default {
         button: '버튼'
       },
 
-      number : [1, 2, 3, 4, 5]
+      number: [1, 2, 3, 4, 5],
+      todos: [
+        {id: 1, title: 'javascript'},
+        {id: 2, title: 'vue.js'},
+        {id: 3, title: 'react'}
+      ],
+
+      newTodoText: '',
+      nextTodoId: 4,
     };
   },
   computed: {
@@ -44,7 +72,16 @@ export default {
     }
   },
   methods: {
-
+    addNewTodo() {
+      this.todos.push({
+        id: this.nextTodoId += 1,
+        title: this.newTodoText
+      })
+      this.newTodoText = ''
+    },
+    remove(index) {
+      return this.todos.splice(index, 1)
+    }
   },
 
 };

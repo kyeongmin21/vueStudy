72745bba78672faec2874cc2f4895746f3750a7c
kim kyeong min <kyeongmin21@naver.com>
2022-03-15 21:38:54 +0900

    data 넣기

--- src/views/Data.vue
+++ src/views/Data.vue
@@ -1,13 +1,9 @@
 <template>
   <div>
     <h1>data / methods</h1>
-
-    <section class="first">
-
-    </section>
-
     <section>
-
+      <p>{{ name }} : {{ age }}</p>
+      <p>{{ person.fullName}}이의 생일은 {{ person.birthday }}입니다.</p>
     </section>
   </div>
 </template>
@@ -16,7 +12,12 @@
 export default {
   data() {
     return {
-
+      name: '민',
+      age: '20',
+      person: {
+        fullName: '경민',
+        birthday: '1011'
+      }
     };
   },
 };

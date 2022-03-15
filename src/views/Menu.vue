c3a994470c05ffcddae81228669ca0b8fdba4118
kim kyeong min <kyeongmin21@naver.com>
2022-03-15 21:34:07 +0900

    router-link 수정

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -2,8 +2,8 @@
   <div class="about">
     <div id="nav">
       <ul class="category">
-        <li><h2><router-link to="/model">1. data/methods</router-link></h2></li>
-        <li><h2><router-link to="/class">2. v-bind</router-link></h2></li>
+        <li><h2><router-link to="/data">1. data/methods</router-link></h2></li>
+        <li><h2><router-link to="/bind">2. v-bind</router-link></h2></li>
         <li><h2><router-link to="/model">3. v-model</router-link></h2></li>
         <li><h2><router-link to="/if">4. v-if / v-show</router-link></h2></li>
         <li><h2><router-link to="/for">5. v-for</router-link></h2></li>
@@ -16,8 +16,8 @@
         <li><h2><router-link to="/parent">10. parent</router-link></h2></li>
       </ul>
       <ul class="category">
-        <li><h2><router-link to="/page" tag="a">9. 페이지 이동</router-link></h2></li>
-        <li><h2><router-link to="/event" tag="a">10. 이벤트 핸들링</router-link></h2></li>
+        <li><h2><router-link to="/page">9. 페이지 이동</router-link></h2></li>
+        <li><h2><router-link to="/event">10. 이벤트 핸들링</router-link></h2></li>
       </ul>
     </div>
   </div>

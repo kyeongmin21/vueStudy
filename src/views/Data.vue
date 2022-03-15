3d908ff0caef8d4dd037a38f9427a8f62159caf7
kim kyeong min <kyeongmin21@naver.com>
2022-03-15 21:46:52 +0900

    methods 사용법

--- src/views/Data.vue
+++ src/views/Data.vue
@@ -5,6 +5,10 @@
       <p>{{ name }} : {{ age }}</p>
       <p>{{ person.fullName}}이의 생일은 {{ person.birthday }}입니다.</p>
     </section>
+    <section>
+      <p>{{ nextYear() }}</p>
+      <p>{{ hello('안녕하세요') }}</p>
+    </section>
   </div>
 </template>
 
@@ -13,13 +17,22 @@ export default {
   data() {
     return {
       name: '민',
-      age: '20',
+      age: 20,
       person: {
         fullName: '경민',
         birthday: '1011'
-      }
+      },
+
     };
   },
+  methods: {
+    nextYear() {
+      return ` ${this.person.fullName}이는 내년에 ${ this.age + 1 }살 입니다.`
+    },
+    hello(say) {
+      return say
+    }
+  }
 };
 </script>
 

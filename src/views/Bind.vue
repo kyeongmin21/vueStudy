733b231739a722cd530bf1bbd2ffa62fe82d0898
kim kyeong min <kyeongmin21@naver.com>
2022-03-15 22:02:27 +0900

    html속성을 이용하여 데이터 바인딩하기

--- src/views/Bind.vue
+++ src/views/Bind.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
     <h1>v-bind</h1>
-
-    <section class="first">
-
+    <section>
+      <input :type="inputType"><br>
+      <input :value="inputValue">
     </section>
 
     <section>
-
+      <a :href="link">네이버</a><br>
+      <a :href="getLink('kossiecoder')">코지코더 유튜브채널</a>
     </section>
   </div>
 </template>
@@ -16,9 +17,17 @@
 export default {
   data() {
     return {
-
+      inputType: 'number',
+      inputValue: 'hello',
+      link: 'https://www.naver.com',
+      youtube: 'https://www.youtube.com/',
     };
   },
+  methods: {
+    getLink(channel) {
+      return this.youtube + channel
+    }
+  }
 };
 </script>
 

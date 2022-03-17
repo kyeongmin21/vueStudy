758507c813089035846748f932ed7ced0cf54b45
kyeongmin1011 <kyeongmin@safety-as.com>
2022-03-17 15:45:42 +0900

    slot 기본

--- src/views/Slot.vue
+++ src/views/Slot.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
     <h1>slot</h1>
-
+    <Button/>
   </div>
 </template>
 
 <script>
+import Button from "@/components/Button"
 export default {
   data() {
     return {
 
     };
+  },
+  components: {
+    Button,
   },
   methods: {
 

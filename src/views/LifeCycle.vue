f32421e85f1cba4673972c82a78eb819b57f18bc
kim kyeong min <kyeongmin21@naver.com>
2022-03-17 22:37:59 +0900

    vue lifecycle

--- src/views/LifeCycle.vue
+++ src/views/LifeCycle.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-
+    {{ name }}
   </div>
 </template>
 
@@ -11,7 +11,26 @@ export default {
       name: 'min'
     };
   },
-
+  beforeCreate() {
+    console.log('beforeCreate', this.name)
+  },
+  created() {  // data 접근이 가능해짐, 서버에 요청을 보내서 응답을 받아 업데이트 해줄때 많이 쓰임
+    console.log('created', this.name)
+  },
+  beforeMount() {
+    console.log('beforeMount')
+    alert('beforeMount')
+  },
+  mounted() {
+    console.log('mounted')
+    alert('mounted')
+  },
+  beforeDestroy() {
+    alert('beforeDestroy')
+  },
+  destroyed() {
+    alert('destroyed')
+  }
 };
 </script>
 

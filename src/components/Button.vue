69c37dbe059b2556641ca28797cebc49bb454ef0
kim kyeong min <kyeongmin21@naver.com>
2022-03-18 22:47:41 +0900

    css연결

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -34,22 +34,6 @@ export default {
 };
 </script>
 
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+<style>
+@import "/css/style.css";
 </style>

db2b968973c9a8860293d178de2ee0a865f4bb51
kyeongmin1011 <kyeongmin@safety-as.com>
2022-03-18 16:57:53 +0900

    computed 메소드로 빼내는법 생각하기

--- src/views/Computed.vue
+++ src/views/Computed.vue
@@ -5,6 +5,20 @@
     <p>{{ reverseMessage }}</p>
     <p>{{ reverseMessage }}</p>
     <p>{{ reverseMessage }}</p>
+
+    <div>
+      <p v-if="typeof number.num1">{{ number.num1 }}</p>
+      <p v-if="number.num2 && !number.num3 && !number.num4">>{{ number.num2 }}</p>
+      <p v-if="!number.num2 && number.num3 && !number.num4">>{{ number.num3 }}</p>
+      <p v-if="!number.num2 && !number.num3 && number.num4">>{{ number.num4 }}</p>
+      <p v-if="!number.num1 && !number.num2 && !number.num3 && !number.num4">{{ number.other }}</p>
+    </div>
+    <hr>
+    <div>
+      <p>{{ test }}</p>
+    </div>
+
+
   </div>
 </template>
 
@@ -12,7 +26,15 @@
 export default {
   data() {
     return {
-      message: '안녕하세요'
+      number: {
+        num1: 0,
+        num2: '',
+        num3: '',
+        num4: '',
+        other: '해당없음'
+      },
+     result: '',
+      message: '안녕하세요',
     };
   },
   methods: {
@@ -21,6 +43,13 @@ export default {
     }
   },
   computed: {
+    test() {
+      const print = []
+      for (const i in this.number) {
+        print.push(this.number[i])
+      }
+      return print.join('')
+    },
       reverseMessage() {
         return this.message.split('').reverse().join('')
       }

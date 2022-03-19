1479f5f4977bf21bee8a4d26e8ab265464e2afc3
kim kyeong min <kyeongmin21@naver.com>
2022-03-19 23:14:35 +0900

    emit

---
+++ src/components/InputField.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <label for="person">Name</label>
+    <input id="person" type="text"
+           :value="name"
+            @input="updateName">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InputFiled",
+  props: {
+    name: {
+      type: String,
+      required: true,
+    }
+  },
+  methods: {
+    updateName(e) {
+      console.log(e.target.value)
+      this.$emit('update-name', e.target.value)
+    }
+  },
+};
+</script>
+
+<style>
+@import "/css/style.css";
+</style>
--- src/views/Props.vue
+++ src/views/Props.vue
@@ -2,23 +2,34 @@
   <div>
     <h1>props, emit</h1>
     <Emit title="타이틀입니다." :content="cont"></Emit>
+    <form>
+      <InputFiled :name="name" @update-name="update"></InputFiled>
+      <button>submit</button>
+    </form>
+    {{ name }}
   </div>
 </template>
 
 <script>
 import Emit from "@/components/Emit";
+import InputFiled from "@/components/InputField";
 
 export default {
   name: 'Props',
   components: {
     Emit,
+    InputFiled,
   },
   data() {
     return {
-      cont: '내용입니다.'
+      cont: '내용입니다.',
+      name: '',
     };
   },
   methods: {
+    update(name) {
+      this.name = name;
+    }
 
   },
 };

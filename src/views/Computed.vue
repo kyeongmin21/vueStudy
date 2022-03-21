197b588df62d84df444d82a0e85423e833f281d9
kyeongmin1011 <kyeongmin@safety-as.com>
2022-03-21 13:46:16 +0900

    코드 삭제

--- src/views/Computed.vue
+++ src/views/Computed.vue
@@ -5,20 +5,6 @@
     <p>{{ reverseMessage }}</p>
     <p>{{ reverseMessage }}</p>
     <p>{{ reverseMessage }}</p>
-
-    <div>
-      <p v-if="typeof number.num1">{{ number.num1 }}</p>
-      <p v-if="number.num2 && !number.num3 && !number.num4">>{{ number.num2 }}</p>
-      <p v-if="!number.num2 && number.num3 && !number.num4">>{{ number.num3 }}</p>
-      <p v-if="!number.num2 && !number.num3 && number.num4">>{{ number.num4 }}</p>
-      <p v-if="!number.num1 && !number.num2 && !number.num3 && !number.num4">{{ number.other }}</p>
-    </div>
-    <hr>
-    <div>
-      <p>{{ test }}</p>
-    </div>
-
-
   </div>
 </template>
 
@@ -26,14 +12,6 @@
 export default {
   data() {
     return {
-      number: {
-        num1: 0,
-        num2: '',
-        num3: '',
-        num4: '',
-        other: '해당없음'
-      },
-     result: '',
       message: '안녕하세요',
     };
   },
@@ -43,13 +21,6 @@ export default {
     }
   },
   computed: {
-    test() {
-      const print = []
-      for (const i in this.number) {
-        print.push(this.number[i])
-      }
-      return print.join('')
-    },
       reverseMessage() {
         return this.message.split('').reverse().join('')
       }

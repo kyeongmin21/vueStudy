84c3d50fe58390c0cc57ff8144eb154bc8eceea2
kyeongmin1011 <kyeongmin@safety-as.com>
2022-04-13 09:06:13 +0900

    bootstrap 설치

--- src/views/Table.vue
+++ src/views/Table.vue
@@ -1,29 +1,21 @@
 <template>
   <div>
     <h1>table 바인딩</h1>
-      <b-table>
-        <template>
-          <b-form-input>
-
-          </b-form-input>
-        </template>
-      </b-table>
+    <b-form-input v-model="text" placeholder="Enter your name">
+    </b-form-input>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'Table',
   data() {
     return {
-
+      text: ''
     };
   },
-  methods: {
-
-  },
-  computed: {
-
-  },
+  methods: {},
+  computed: {},
 
 };
 </script>

dd36e6147992a1245b33a7ec232c990fa7a34b87
kyeongmin1011 <kyeongmin@safety-as.com>
2022-04-22 15:13:49 +0900

    board edit

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -3,24 +3,16 @@
     <h1>board</h1>
     <HelloWorld ref="usernameInput"></HelloWorld>
     <b-button variant="outline-primary" @click="onClick">클릭</b-button>
-    <p>{{ age }}</p>
   </div>
 </template>
 
 <script>
 import HelloWorld from "@/components/HelloWorld"
-import Mixin from "@/mixin/mixin"
 
 export default {
   name: "Board",
-  mixins: [Mixin],
   components: {
-    HelloWorld,
-  },
-  data() {
-    return {
-      age: 20
-    }
+    HelloWorld
   },
   methods: {
     onClick() {
@@ -31,7 +23,3 @@ export default {
   }
 };
 </script>
-
-<style>
-
-</style>

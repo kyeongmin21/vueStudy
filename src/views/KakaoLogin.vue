e5d9a3398597a1ae75efe9347999452be423cf95
kyeongmin1011 <kyeongmin@safety-as.com>
2022-04-26 11:43:40 +0900

    update

--- src/views/KakaoLogin.vue
+++ src/views/KakaoLogin.vue
@@ -1,18 +1,43 @@
 <template>
   <div>
     <h1>kakao login</h1>
+    <a id="custom-login-btn" @click="kakaoLogin()" >
+      <img
+          src="//k.kakaocdn.net/14/dn/btroDszwNrM/I6efHub1SN5KCJqLm1Ovx1/o.jpg"
+          width="222"
+          alt="카카오 로그인 버튼"
+      />
+    </a>
+    <button type="logout"></button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'KakaoLogin',
-  data() {
-    return {
-
-    }
-  },
   methods: {
+    kakaoLogin () {
+      window.Kakao.Auth.login({
+        scope: 'profile_nickname, account_email',
+        success: this.getKakaoAccount
+      })
+    },
+    getKakaoAccount () {
+      window.Kakao.API.request({
+        url: '/v2/user/me',
+        success: res => {
+          const kakao_account = res.kakao_account
+          const nickname = kakao_account.profile.nickname
+          const email = kakao_account.email
+          console.log('nickname', nickname)
+          console.log('email', email)
+          console.log('로그인 성공!')
+        },
+        fail: error => {
+          console.log(error)
+        }
+      })
+    }
   }
 }
 </script>

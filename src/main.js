2623cef95a6b0d6b364e919b19b8d6822f64c4e8
kim kyeong min <kyeongmin21@naver.com>
2022-04-28 08:22:37 +0900

    fontawesome 라이브러리 import

--- src/main.js
+++ src/main.js
@@ -6,9 +6,15 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.use(BootstrapVue)
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fas)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
 
+Vue.use(BootstrapVue)
 new Vue({
   router,
   store,

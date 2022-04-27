aaace513f34471e6efa41b3af12390ccfe8849ae
kim kyeong min <kyeongmin21@naver.com>
2022-04-28 08:23:18 +0900

    카카오 로그인 지도 api

--- src/views/KakaoLogin.vue
+++ src/views/KakaoLogin.vue
@@ -1,15 +1,21 @@
 <template>
-  <div>
-    <h1>kakao login</h1>
-    <a id="custom-login-btn" @click="kakaoLogin()" >
-      <img
-          src="//k.kakaocdn.net/14/dn/btroDszwNrM/I6efHub1SN5KCJqLm1Ovx1/o.jpg"
-          width="222"
-          alt="카카오 로그인 버튼"
-      />
-    </a>
-    <button type="logout"></button>
-  </div>
+  <div class="kakao">
+    <h1>Kakao</h1>
+    <div id="custom-login-btn" @click="kakaoLogin()">
+      <button type="button" class="kakaoBtn">
+        <font-awesome-icon icon="fa-solid fa-comments"/>
+        카카오 로그인
+      </button>
+      <button type="button"
+              @click="kakaoLogout()"
+              class="kakaoLogout kakaoBtn ml-1"> 로그아웃
+      </button>
+    </div>
+
+    <div class="kakaoMap">
+      <div id="map"></div>
+    </div>
+  </div><!--// kakao -->
 </template>
 
 <script>
@@ -17,14 +23,24 @@ window.Kakao.init('3d11e9ddcd1b78eee6b8dec29e8583e4')
 
 export default {
   name: 'KakaoLogin',
+  mounted() {
+    if (window.kakao && window.kakao.maps) {
+      this.initMap()
+    } else {
+      const script = document.createElement('script')
+      script.onload = () => kakao.maps.load(this.initMap)
+      script.src = 'http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=3d11e9ddcd1b78eee6b8dec29e8583e4'
+      document.head.appendChild(script)
+    }
+  },
   methods: {
-    kakaoLogin () {
+    kakaoLogin() {
       window.Kakao.Auth.login({
         scope: 'profile_nickname, account_email',
         success: this.getKakaoAccount
       })
     },
-    getKakaoAccount () {
+    getKakaoAccount() {
       window.Kakao.API.request({
         url: '/v2/user/me',
         success: res => {
@@ -39,6 +55,19 @@ export default {
           console.log(error)
         }
       })
+    },
+    kakaoLogout() {
+      window.Kakao.Auth.logout((res) => {
+        console.log(res)
+      })
+    },
+    initMap() {
+      const mapContainer = document.getElementById('map'),
+        mapOption = {
+          center: new kakao.maps.LatLng(37.564343, 126.947613),
+          level: 3
+        }
+      const map = new kakao.maps.Map(mapContainer, mapOption)
     }
   }
 }

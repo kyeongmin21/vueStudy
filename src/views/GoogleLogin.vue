b15b4730cb702708b3a8edb9f794b04745fa7ff5
kyeongmin1011 <kyeongmin@safety-as.com>
2022-05-02 08:59:28 +0900

    구글 페이지 추가

---
+++ src/views/GoogleLogin.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="google">
+    <h1>google</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GoogleLogin'
+}
+</script>
+
+<style>
+@import "/css/style.css";
+</style>

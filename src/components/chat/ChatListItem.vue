8c6433d7cf65df299d994e544ea6ff8eebf64542
kim kyeong min <kyeongmin21@naver.com>
2022-05-02 21:37:03 +0900

    chat 폴더 생성

---
+++ src/components/chat/ChatListItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChatListItem"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,12 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    chatList: [
+      { id: 1, lastMessage: '채팅메세지1', new: 1},
+      { id: 2, lastMessage: '채팅메세지2', new: 2},
+      { id: 3, lastMessage: '채팅메세지3', new: 1},
+      { id: 4, lastMessage: '채팅메세지4', new: 5},
+    ]
   },
   mutations: {
   },

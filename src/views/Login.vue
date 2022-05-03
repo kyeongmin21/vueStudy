a227229c8df3ae65eb01c2e0efde5724ec5f9bb9
kyeongmin1011 <kyeongmin@safety-as.com>
2022-05-03 09:13:50 +0900

    axios를 통한 로그인구현

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,35 @@
 <template>
   <div>
+    <input type="email" placeholder="email을 입력해주세요." id="email" value="">
+    <input type="password" placeholder="password를 입력해주세요" id="password" value="">
 
+    <b-button type="button" @click="login">로그인</b-button>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: "Login"
+  name: "Login",
+  methods: {
+    login() {
+      const email = document.getElementById('email')
+      const password = document.getElementById('password')
+      axios({
+        method: "POST",
+        url: 'https://reqres.in/api/login',
+        data: {
+          "email" : email.value,
+          "password" : password.value
+        }
+      }).then(res => {
+        console.log(res)
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+  }
 }
 </script>
 

b1fd2d059f29f1d7dcc971ddcef5de5295a06fb4
kyeongmin1011 <kyeongmin@safety-as.com>
2022-05-09 09:00:23 +0900

    가위바위보를 내가 선택하면 컴퓨터도 랜덤으로 가위바위보 나오게 하기

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -2,14 +2,28 @@
   <div class="home">
     <div class="row">
       <div class="small-5 columns text-center">
-        <img src="images/rock.jpg" alt="" class="text-center">
-        <h1 class="text-center"><strong>YOU</strong></h1>
+        <img v-if="myChoice !== null"
+             :src="'images/' + myChoice + '.jpg'"
+             alt="가위바위보이미지"
+             class="text-center">
+        <img v-else
+             src="images/question.jpg"
+             alt="물음표이미지"
+             class="text-center">
+        <h1 class="text-center"><strong>Min</strong></h1>
       </div>
       <div class="small-2 columns text-center">
-        <h1 style="font-size:100px;"><strong>3</strong></h1>
+        <h1 style="font-size:100px;"><strong>{{ count }}</strong></h1>
       </div>
       <div class="small-5 columns text-center">
-        <img src="images/question.jpg" alt="" class="text-center">
+        <img v-if="comChoice !== null"
+             :src="'images/' + comChoice + '.jpg'"
+             alt="가위바위보이미지"
+             class="text-center">
+        <img v-else
+             src="images/question.jpg"
+             alt="물음표이미지"
+             class="text-center">
         <h1 class="text-center"><strong>Computer</strong></h1>
       </div>
     </div>
@@ -17,16 +31,16 @@
     <div class="row">
       <div class="small-6 columns text-center">
         <div class="battle-wrap">
-          <img src="./images/heart.jpg" class="heart" alt="">
-          <img src="./images/heart.jpg" class="heart" alt="">
-          <img src="./images/heart.jpg" class="heart" alt="">
+          <img src="/images/heart.jpg" class="heart" alt="">
+          <img src="/images/heart.jpg" class="heart" alt="">
+          <img src="/images/heart.jpg" class="heart" alt="">
         </div>
       </div>
       <div class="small-6 columns text-center">
         <div class="battle-wrap">
-          <img src="./images/heart.jpg" class="heart" alt="">
-          <img src="./images/heart.jpg" class="heart" alt="">
-          <img src="./images/heart.jpg" class="heart" alt="">
+          <img src="/images/heart.jpg" class="heart" alt="">
+          <img src="/images/heart.jpg" class="heart" alt="">
+          <img src="/images/heart.jpg" class="heart" alt="">
         </div>
       </div>
     </div>
@@ -35,15 +49,15 @@
       <div class="small-6 columns text-center">
         <div class="row">
           <div class="small-8 small-offset-2 columns text-center">
-            <label class="radio-label"><input type="radio"> 가위</label>
-            <label class="radio-label"><input type="radio"> 바위</label>
-            <label class="radio-label"><input type="radio"> 보</label>
+            <label class="radio-label"><input type="radio" v-model="myChoice" value="scissor"> 가위</label>
+            <label class="radio-label"><input type="radio" v-model="myChoice" value="rock"> 바위</label>
+            <label class="radio-label"><input type="radio" v-model="myChoice" value="paper"> 보</label>
           </div>
         </div>
         <div class="row">
           <div class="small-12 columns">
             <div class="text-center">
-              <button class="start-btn">선택 완료!</button>
+              <button class="start-btn" @click="startGame">선택 완료!</button>
             </div>
             <div class="loading"> 기다리는 중...</div>
           </div>
@@ -71,11 +85,38 @@ export default {
   name: 'Game',
   data() {
     return {
+      myChoice: null,
+      comChoice: null,
+      count: 3
     }
   },
   watch: {
+    count(newVal) {
+      if (newVal === 0) {
+        let number = Math.trunc(Math.random() * 10)
+        if (number < 3) {
+          this.comChoice = 'scissor'
+        } else if(number < 6) {
+          this.comChoice = 'rock'
+        } else {
+          this.comChoice = 'paper'
+        }
+      }
+    }
   },
   methods: {
+    startGame() {
+      if (this.myChoice === null) {
+        alert('가위 바위 보를 선택해주세요.')
+      } else {
+       let countDown = setInterval(() => {
+          this.count--
+         if (this.count === 0) {
+           clearInterval(countDown)
+         }
+        }, 500)
+      }
+    }
   }
 }
 </script>

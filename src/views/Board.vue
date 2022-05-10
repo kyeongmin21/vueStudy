9af0db23022b5a1ab930baf847bbe8ad4a63eb86
kyeongmin1011 <kyeongmin@safety-as.com>
2022-05-10 15:46:02 +0900

    board추가

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,32 +1,11 @@
 <template>
   <div>
-    <Header :new-message-count="newMessageCount"/>
-    <div class="container mx-auto mt-5"></div>
-    <ChatList :chat-list="chatList" />
+
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import Header from '@/components/chat/Header'
-import ChatList from '@/components/chat/ChatList'
-
 export default {
-  name: "Chat",
-  components: {
-    Header,
-    ChatList
-  },
-  computed: {
-    newMessageCount () {
-      return this.chatList.map(item => item.new).reduce((a, b) => a + b)
-    },
-    // spread 스프레드 연산자 배열,json 합쳐줌
-    ...mapState({
-      chatList(state) {
-        return state.chatList.filter(chat => chat.new >= 1)
-      }
-    })
-  }
+  name: "Board"
 }
 </script>

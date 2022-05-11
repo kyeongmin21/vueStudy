c431537502441b44485fb51c7c93be73f6837497
kyeongmin1011 <kyeongmin@safety-as.com>
2022-05-12 08:14:42 +0900

    List, ListAdd 컴포넌트 import

--- src/components/ListAdd.vue
+++ src/components/ListAdd.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    <b-button class="text-left mt-2 d-block">할일 추가</b-button>
   </div>
 </template>
 
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,11 +1,26 @@
 <template>
-  <div>
+  <div class="container">
+    <div class="row">
+      <div class="col-6">
+        <List/>
+      </div>
+      <div class="col-6">
+        <ListAdd/>
+      </div>
+    </div>
 
   </div>
 </template>
 
 <script>
+import List from '@/components/List'
+import ListAdd from '@/components/ListAdd'
+
 export default {
-  name: "Todo"
+  name: "Todo",
+  components: {
+    List,
+    ListAdd
+  }
 }
 </script>

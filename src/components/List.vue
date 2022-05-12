a674ea20b8b6e49264db03162d09e186f8800159
kyeongmin1011 <kyeongmin@safety-as.com>
2022-05-12 11:12:58 +0900

    완료/부활버튼 v-if, v-else문으로 처리

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
     <div class="card mb-2 p-2"
+         :class="{ done: list.status === 'done' }"
          v-for="(list, idx) in todoList"
          :key="idx">
       <span>할일 : {{ list.memo }}</span>
 
       <div class="todoBtn">
-        <button @click="completeBtn(idx)" class="btn-outline-success inline">완료</button>
-        <button @click="restoreBtn(idx)" class="btn-outline-warning">부활</button>
+        <button v-if="list.status === 'created'" @click="completeBtn(idx)" class="btn-outline-success inline">완료</button>
+        <button v-else @click="restoreBtn(idx)" class="btn-outline-warning">부활</button>
         <button @click="deleteBtn(idx)" class="btn-outline-danger">제거</button>
       </div>
 
@@ -47,5 +48,8 @@ export default {
     border: 1px solid #dadada;
     margin: 5px;
     border-radius: 10px;
+  }
+  .done {
+    background-color: rgba(0,0,0,.1);
   }
 </style>
